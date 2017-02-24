78ff03e0e62440f073d262fcf0623a915ee05350
Andrei Tsviatsinski <atsviatsinski@edmunds.com>
2017-02-24 11:11:20 +0300

    add comments to base webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 const path = require('path');
 
 module.exports = {
+	// start point to resolve dependencies 
 	context: path.resolve(__dirname,'src'),
+
+	// MUST: start point to resolve
 	entry: './js/app.js',
+
+	// MUST: result of work
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build')
 	},
+
+	// extensions to work with
 	resolve: {
 		extensions: ['.js']
 	},
+
+	// Watch the filesystem for changes
 	watch: false
 }
